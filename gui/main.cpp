210929bf3c970e8c9cc2111579a412f490f55a05
KeithBoehler <germanbohler@gmail.com>
2019-11-16 12:43:49 -0600

    Forgot this for last commit

--- gui/main.cpp
+++ gui/main.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <cstdlib>
 #include <sqlite3.h>
+
+
 #include <string>
 #include <QApplication>
 
@@ -10,15 +12,15 @@ int main(int argc, char *argv[])
 {
     std::cout << "Hi" << std::endl;
     //std::string path = "/Users/keithnator3000/OneDrive - The University of Texas-Rio Grande Valley/Classes/STEM/Computer_Science/Software Engineering I - CSCI 3340/SE_Fall19/project/SE_Meeting_Project/sqlite/where2meet.db";
-    std::string path = "/home/keithnator3000/SE_Meeting_Project";
+    std::string path = "/home/keithnator3000/SE_Meeting_Project/sqlite/where2meet.db";
     Database DBObj(path);
     //Database DBObj;
-    DBObj.createAccountData();
-    int id = DBObj.loginAccount();
-    DBObj.insertSchedualData(id);
-    DBObj.getAccountData(id);
-    DBObj.getSchedualData(id);
-    DBObj.closeDB();
+    //DBObj.createAccountData();
+    //int id = DBObj.loginAccount();
+    //DBObj.insertSchedualData(id);
+    //DBObj.getAccountData(id);
+    //DBObj.getSchedualData(id);
+    //DBObj.closeDB();
     std::cout << "Bye!" << std::endl;
     QApplication a(argc, argv);
     LoginWindow w;
